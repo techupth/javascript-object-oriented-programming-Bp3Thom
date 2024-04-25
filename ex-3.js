e45744bfa4d15dd9b9eebde7b028e1897bc5e133
bp3_thom <thompolo@gmail.com>
2024-04-25 14:26:21 +0700

    fix: ex3 facebook

--- ex-3.js
+++ ex-3.js
@@ -7,49 +7,53 @@ class User {
     }
 }
 class PostList {
-    constructor(posts) {
-        this.posts = posts = [];
+    constructor() {
+        this.posts = [];
     }
-    addpost() {
-        this.posts.push(posts);
+    addpost(post) {
+        this.posts.push({
+            id: this.posts.length + 1,
+            title: post.title,
+            content: post.content,
+            comment: post.comment,
+        });
     }
-    sharePost() {
-        console.log(`You've shared post ${this.title} to your friend.`)
+    sharePost(postId) {
+        console.log(`You've shared post "${this.posts[postId-1].title}" to your friend.`)
     }
 }
 class Post {
-    constructor(id, title, content, comment) {
+    constructor(id, title, content) {
         this.id = id;
         this.title = title;
         this.content = content;
-        this.comment = comment;
+        this.comment = [];
     }
-    addComment() {
-        this.comment.push(new Comment(this.id, this.title, this.content, this.comment))
+    addComment(comments) {
+        this.comment.push(comments);
     }
 }
 class Comment {
-    constructor(id, content, createdBy, like) {
+    constructor(id, content, createdBy) {
         this.id = id;
         this.content = content;
         this.createdBy = createdBy;
-        this.like = like;
+        this.like = 0;
     }
     addLike() {
-        const like = 1;
-        this.like += like;
+        this.like = this.like + 1;
     }
 }
 class Facebook {
-    constructor(groupList, pageList) {
-        this.groupList  = groupList;
-        this.pageList = pageList;
+    constructor() {
+        this.groupList  = [];
+        this.pageList = [];
     }
-    addGroup() {
-        this.groupList.push(addGroup());
+    addGroup(group) {
+        this.groupList.push(group);
     }
-    addPage() {
-        this.pageList.push(addPage())
+    addPage(page) {
+        this.pageList.push(page)
     }
 }
 class FacebookPage {
@@ -66,7 +70,62 @@ class Notification {
     constructor(id) {
         this.id = id;
     }
-    send() {
-        console.log(`Notification: ${this.createdBy} has just commented on this post-${this.title}`)
+    send(comment,post) {
+        console.log(`Notification: ${comment.createdBy.name} has just commented on this post-${post.title}`)
     }
 }
+
+//สร้าง User
+const jane = new User("001", "Jane", "jane@gmail.com");
+
+//สร้าง Post List
+const postList = new PostList();
+
+//สร้าง Post
+const firstPost =  new Post(1, "My first post", "Hello !!");
+
+//สร้าง Comment 
+const firstComment = new Comment(1, "Greeting !!", jane);
+//เพิ่ม Like ของ comment
+firstComment.addLike();
+firstComment.addLike();
+//ดู Like ของ comment
+console.log(firstComment.like)
+
+//เพิ่ม Post เข้าไปใน Post list
+postList.addpost(firstPost);
+
+//ดูรายการ Post ทั้งหมด
+console.log(postList.posts)
+
+//เพิ่ม comment เข้าไปใน post
+firstPost.addComment(firstComment);
+
+// Share post ที่ไอดีเป็น (1)
+postList.sharePost(1);
+
+//สร้าง facebook
+const facebook = new Facebook();
+
+//สร้าง page
+const firstpage = new FacebookPage("My first page");
+
+//สร้าง group
+const firstGroup = new FacebookGrouop("My first group");
+//เพิ่ม firstpage เข้าไปใน facebook
+facebook.addPage(firstpage);
+//เพิ่ม firstgroup เข้าไปใใน facebook
+facebook.addGroup(firstGroup);
+
+//ดูรายการ facebook page และ facebook group
+console.log(facebook.pageList)
+console.log(facebook.groupList)
+
+//สร้าง Notification 
+const notification = new Notification(1);
+//ส่ง Notification
+notification.send(firstComment, firstPost);
+
+
+
+
